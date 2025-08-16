71556ff7bd224123be951edbcd0e0f3d44a392d6
AJ <21494163@student.curtin.edu.au>
2025-08-16 21:33:36 +0800

    Initial test messages and setting up a testing server to begin complete development

--- src/main.py
+++ src/main.py
@@ -1,3 +1,35 @@
-import os
 import discord
+import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+CHANNEL_NAME = os.getenv('CHANNEL_NAME')
+
+intents = discord.Intents.default()
+intents.guilds = True
+intents.messages = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    
+    channel = discord.utils.get(guild.text_channels, name=CHANNEL_NAME)
+    if channel is None:
+        print(f"Channel '{CHANNEL_NAME}' not found")
+        return
+
+    # Send the message
+    await channel.send("Hello, this is a test message!")
+
+client.run(TOKEN)
