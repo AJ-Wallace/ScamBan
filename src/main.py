c02ed5de07d0b4ad29bea3ea9c05e3cfe285282c
AJ <21494163@student.curtin.edu.au>
2025-08-17 10:41:36 +0800

    - Message activation system that extracts the details - Beginning to set up the AI classification model running locally

--- src/main.py
+++ src/main.py
@@ -2,34 +2,34 @@ import discord
 import os
 
 from dotenv import load_dotenv
+from transformers import pipeline
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-CHANNEL_NAME = os.getenv('CHANNEL_NAME')
+
 
 intents = discord.Intents.default()
 intents.guilds = True
 intents.messages = True
+intents.message_content = True
 client = discord.Client(intents=intents)
+
+print("Loading Classification Model...")
+classifier = pipeline("text-classification", model="ealvaradob/bert-finetuned-phishing")
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    print(f"Successfully Logged In As {client.user}")
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-    
-    channel = discord.utils.get(guild.text_channels, name=CHANNEL_NAME)
-    if channel is None:
-        print(f"Channel '{CHANNEL_NAME}' not found")
-        return
-
-    # Send the message
-    await channel.send("Hello, this is a test message!")
+@client.event
+async def on_message(msg):
+    if (msg.author != client.user):
+        # print(f"Author: {msg.author}")
+        # print(f"Date/Time: {msg.created_at.astimezone()}")
+        # print(f"Message: {msg.content}")
+        # print()
+        result = classifier(msg.content)
+        print(result)
 
 client.run(TOKEN)
